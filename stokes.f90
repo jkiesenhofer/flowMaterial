b8629b409d60c87b8e10eda7a1855ac6eef86f60
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-26 21:50:43 +0100

    Update stokes.f90

--- stokes.f90
+++ stokes.f90
@@ -1,19 +1,21 @@
 program stokeszahl
   implicit none
   integer :: i
-  real(8) :: dp, rho_p, mu, U, L, St
+  real(8) :: dp, rho_p, mu, U, L, St1, St2
 
   rho_p = 2500.0d0      ! Partikeldichte [kg/m^3]
   mu    = 1.8d-5        ! dyn. Viskosität Luft [Pa s]
-  U     = 1.0d0         ! Strömungsgeschwindigkeit [m/s]
+  U1    = 2.0d0         ! Strömungsgeschwindigkeit [m/s]
+  U2    = 5.0d0         ! Strömungsgeschwindigkeit [m/s]
   L     = 0.1d0         ! charakteristische Länge [m]
 
   open(unit=10, file="stokes.dat", status="replace")
 
   do i = 1, 100
      dp = i * 1.0d-6    ! Partikeldurchmesser [m]
-     St = rho_p * dp**2 * U / (18.0d0 * mu * L)
-     write(10,*) dp, St
+     St1 = rho_p * dp**2 * U1 / (18.0d0 * mu * L)
+     St2 = rho_p * dp**2 * U2 / (18.0d0 * mu * L)
+     write(10,*) dp, St1, St2
   end do
 
   close(10)
