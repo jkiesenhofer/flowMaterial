7aeba5ddf8d4cf0c4821db7ecc8198f6237be33a
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 15:33:13 +0100

    Update stre.m

--- stre.m
+++ stre.m
@@ -13,8 +13,8 @@ dp = i * 1e-6;     % Partikeldurchmesser [m]
 db = i * 1e-4;     % Blasendurchmesser [m]
 
 % Kennzahlen
-St  = rho_p .* dp.^2 .* Ub ./ (18 * mu * L); & Partikelträgheit zur Strömungsgeschwindigkeit des Fluids
-Reb = Ub .* db .* rho_b ./ mu;
+St  = rho_p .* dp.^2 .* Ub ./ (18 * mu * L); % Partikelträgheit zur Strömungsgeschwindigkeit des Fluids
+Reb = Ub .* db .* rho_b ./ mu;               % Trägheitskräfte zu viskosen Kräften
 
 % Verhältnis
 ratio = St ./ Reb;
