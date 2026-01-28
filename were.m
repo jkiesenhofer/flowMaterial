fd6023c5950e734ca7befaf04237240f7e455be7
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 15:28:22 +0100

    Update were.m

--- were.m
+++ were.m
@@ -4,12 +4,13 @@ mu = 1e-3;         % dyn. Viskosität [Pa s]
 sigma = 0.072;     % Oberflächenspannung [N/m]
 L = 1e-3;          % charakteristische Länge [m]
 
+
 % Geschwindigkeit
 v = linspace(0.01, 5, 100);
 
 % Reynolds- und Weber-Zahl
-Re = rho .* v .* L ./ mu;
-We = rho .* v.^2 .* L ./ sigma;
+Re = rho .* v .* L ./ mu;       % Trägheitskräfte zu viskosen Kräften
+We = rho .* v.^2 .* L ./ sigma; % Trägheitskräften zu Oberflächenspannungskräften
 
 % Plot
 figure
