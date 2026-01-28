440be754f8ff28390b43051325800bbfd74bb0b2
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-01-28 15:11:02 +0100

    Create weeo.m

---
+++ weeo.m
@@ -0,0 +1,20 @@
+% Physikalische Parameter
+rho = 1.27;        % Dichte [kg/m^3]
+delta_rho = 6900;  % Dichtedifferenz [kg/m^3]
+sigma = 0.072;     % Oberflächenspannung [N/m]
+g = 9.81;          % Erdbeschleunigung [m/s^2]
+v = 0.5;           % Geschwindigkeit [m/s]
+
+% Charakteristische Länge
+L = linspace(1e-4, 1e-2, 200); % [m]
+
+% Dimensionslose Kennzahlen
+Eo = (delta_rho .* g .* L.^2) ./ sigma;
+We = (rho .* v.^2 .* L) ./ sigma;
+
+% Plot
+figure
+plot(Eo, We, 'LineWidth', 2)
+xlabel('Eötvös-Zahl (Eo)')
+ylabel('Weber-Zahl (We)')
+grid on
