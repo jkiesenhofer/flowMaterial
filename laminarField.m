84ed81f834b62429d214a7a1599ccde66676086a
jkiesenhofer <jkxf489vyn@privaterelay.appleid.com>
2026-02-01 19:31:06 +0100

    Update laminarField.m

--- laminarField.m
+++ laminarField.m
@@ -8,7 +8,7 @@ dx = Lx/(nx-1);
 dy = Ly/(ny-1);
 
 % Parameter
-rho = 1000;
+rho = 1000; % kg/m^3
 nu = 1e-3;
 dt = 0.001;
 nt = 8;
@@ -38,25 +38,15 @@ for n = 1:nt
         + dt/dy^2 * (vn(3:end,2:end-1) - 2*vn(2:end-1,2:end-1) + vn(1:end-2,2:end-1)));
 
     % Boundaries
-    u(20:end,end-20) = -2;
-    %v(25:30,17:25) = 7; % m/s
-    %v(:,end-20:end) = 7;
-    %p(17:25,17:25) = 2e+5;% p(end,:) = rand; N/m^2
-    p(17:25,17:25) = 1.3e+5;
-    %p(10:15,10:15) = rand;
+    v(26:end,end-20) = -5;
+    p(17:25,17:25) = 1.1e+5;
+
 end
+
 speed = sqrt(u.^2 + v.^2);
 Re=rho * speed .* Lx / nu;
-div = u./dx + v./dy;
-%Re=Re/max(max(Re));
+divU = u./dx + v./dy;
 
-%subplot(1,2,1)
-%contourf(Re)
-%colorbar
-%xlabel('x')
-%ylabel('y')
-
-%subplot(1,2,2)
 contourf(speed)
 %quiver(u,v)
 xlim([0 nx])
